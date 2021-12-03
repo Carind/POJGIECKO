5bec4c69977a34bd734297034993fda1daa7fcf1
Carind <35310109+Carind@users.noreply.github.com>
2021-12-03 11:49:19 +0100

    teraz używa ListArray

--- L07/TestInstrumenty.java
+++ L07/TestInstrumenty.java
@@ -3,13 +3,13 @@ import java.util.*;
 //package pl.imiajd.giecko;
 public class TestInstrumenty{
     public static void main(String[] args){
-        Instrument[] orkiestra = new Instrument[5];
+        ArrayList<Instrument> orkiestra = new ArrayList<Instrument>();
 
-        orkiestra[0] = new Flet("Yamaha", LocalDate.of(2010,2,15));
-        orkiestra[1] = new Fortepian("Seiler",LocalDate.of(1970,2,16));
-        orkiestra[2] = new Skrzypce("Stainer",LocalDate.of(1970,2,16));
-        orkiestra[3] = new Skrzypce("Stainer",LocalDate.of(2005,2,16));
-        orkiestra[4] = new Flet("Yamaha",LocalDate.of(2017,2,16));
+        orkiestra.add (new Flet("Yamaha", LocalDate.of(2010,2,15)));
+        orkiestra.add (new Fortepian("Seiler",LocalDate.of(1970,2,16)));
+        orkiestra.add (new Skrzypce("Stainer",LocalDate.of(1970,2,16)));
+        orkiestra.add (new Skrzypce("Stainer",LocalDate.of(2005,2,16)));
+        orkiestra.add (new Flet("Yamaha",LocalDate.of(2017,2,16)));
 
         for (Instrument p : orkiestra) {
             System.out.println(p.dzwiek());
